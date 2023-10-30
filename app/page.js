11079f5793a6091347af8ee537b1791e9c2168db
ac-uneekdigital <anthony@uneekdigital.co.uk>
2023-10-30 09:41:13 +0000

    layout inital

--- app/page.js
+++ app/page.js
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex flex-col h-screen items-center p-16">
+      <h1 className='text-slate-900 font-bold text-4xl'>Todo&apos;s</h1>
+      <div className='h-auto w-6/12 m-4 bg-slate-200 rounded-lg shadow-md p-8'></div>
     </main>
   )
 }
