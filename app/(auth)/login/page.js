6d3d7bbc5aa12e95ca803cc74f6d05d6d1fd00a3
ac-uneekdigital <anthony@uneekdigital.co.uk>
2023-10-30 16:15:07 +0000

    login page

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -69,10 +69,10 @@ export default function Login() {
             <div className="h-[calc(100vh-70px)] w-full flex items-center justify-center z-50">
                 <div className="w-25 p-12 bg-dark/95 rounded-xl">
                     <div className="w-full flex justify-center">
-                        <button className="flex items-center gap-3 text-white border-2 border-white rounded-xl" onClick={signInWithGoogle}>Log In with google</button>
+                        <button className="flex items-center gap-3 text-black border-2 border-black p-4 rounded-xl" onClick={signInWithGoogle}>Log In with google</button>
                     </div>
                     <div className="border-b-4 border-slate-50 py-4"></div>
-                    <p className="text-center py-8 text-white text-base">or log in with your email below</p>
+                    <p className="text-center py-8 text-black text-base">or log in with your email below</p>
                     <LoginForm handleSubmit={handleSubmit} />
                     {error && <p className="error">{error}</p>}
                 </div>
