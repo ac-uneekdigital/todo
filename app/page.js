4bf658637292d79fb748e34284bd6fbb80e8fff5
ac-uneekdigital <anthony@uneekdigital.co.uk>
2023-10-30 09:50:53 +0000

    added search form

--- app/page.js
+++ app/page.js
@@ -4,7 +4,19 @@ export default function Home() {
   return (
     <main className="flex flex-col h-screen items-center p-16">
       <h1 className='text-slate-900 font-bold text-4xl'>Todo&apos;s</h1>
-      <div className='h-auto w-6/12 m-4 bg-slate-200 rounded-lg shadow-md p-8'></div>
+      <div className='h-auto w-6/12 m-4 bg-slate-200 rounded-lg shadow-md p-8'>
+        <form
+          className='flex justify-center'>
+          <input
+            type="text"
+            name="search"
+            className="h-12 w-11/12 bg-white rounded-lg indent-3"
+            placeholder="search for a todo"
+          >
+          </input>
+        </form>
+
+      </div>
     </main>
   )
 }
