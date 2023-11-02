842109e999ab5ca73fe32adee0ad26778df792fa
ac-uneekdigital <anthony@uneekdigital.co.uk>
2023-11-02 15:45:43 +0000

    updating UI on checkbox

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 //icons
 import { FaTrash } from "react-icons/fa";
 import { FaEdit } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 
 function Todo({ todo, onDelete }) {
   const supabase = createClientComponentClient();
@@ -30,7 +31,7 @@ function Todo({ todo, onDelete }) {
     <div className="flex gap-4 items-center rounded-md text-white bg-indigo-800 dark:bg-slate-800 h-20 w-full p-1">
       <p className="text-2xl">{todo.task}</p>
       <FaEdit
-        className="text-white-400 hover:text-indigo-300 cursor-pointer ml-auto"
+        className="text-indigo-400 hover:text-indigo-300 cursor-pointer ml-auto"
         size={36}
         onClick={(e) => {
           e.preventDefault();
@@ -38,14 +39,17 @@ function Todo({ todo, onDelete }) {
           onDelete();
         }}
       />
-      <input
-        className="cursor-pointer checked:accent-green-300 h-8 w-8"
-        onChange={(e) => toggle()}
-        type="checkbox"
-        checked={isCompleted ? true : false}
-      />
+      <div className="flex justify-center items-center">
+        <input
+          className="appearance-none peer cursor-pointer bg-indigo-400 hover:bg-indigo-300 checked:accent-green-300 h-8 w-8"
+          onChange={(e) => toggle()}
+          type="checkbox"
+          checked={isCompleted ? true : false}
+        />
+        <FaCheck className="cursor-pointer absolute hidden peer-checked:block" />
+      </div>
       <FaTrash
-        className="text-red-400 hover:text-red-600 cursor-pointer mr-2"
+        className="text-red-400 hover:text-red-500 cursor-pointer mr-2"
         size={33}
         onClick={(e) => {
           e.preventDefault();
