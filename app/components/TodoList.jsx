36bb3e63ad688184a681e4df85caed9be0b2816c
ac-uneekdigital <anthony@uneekdigital.co.uk>
2023-11-06 09:46:10 +0000

    updated todo list to inc add

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,13 +1,15 @@
 "use client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Todo from "./Todo";
-import AddTodo from "./AddTodo";
+//import AddTodo from "./AddTodo";
 
 function TodoList({ authUser }) {
+  const supabase = createClientComponentClient();
   const [todos, setTodos] = useState([]);
   const [filteredTodos, setfilteredTodos] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const [task, setTask] = useState("");
 
   async function fetchData() {
     const supabase = createClientComponentClient();
@@ -22,6 +24,19 @@ function TodoList({ authUser }) {
     }
   }
 
+  async function handleAddTodo(e) {
+    e.preventDefault();
+    const { error } = await supabase.from("todos").insert({
+      user_id: authUser.id,
+      task: task,
+      is_complete: false,
+      inserted_at: new Date().toISOString(),
+    });
+    setTask("");
+    const newtodos = fetchData();
+    setTodos(newtodos);
+  }
+
   const deleteTodo = async (id) => {
     const supabase = createClientComponentClient();
     try {
@@ -34,11 +49,30 @@ function TodoList({ authUser }) {
 
   useEffect(() => {
     fetchData();
-  }, [todos]);
+  }, []);
 
   return (
     <>
-      <AddTodo authUser={authUser} />
+      <form className="flex flex-col items-center gap-5 w-full pb-2">
+        <input
+          className="h-12 rounded-md text-center w-full lg:w-1/2 self-center"
+          type="text"
+          name="task"
+          placeholder="Enter new task here..."
+          onFocus={(e) => (e.target.placeholder = "")}
+          onBlur={(e) => (e.target.placeholder = "Enter new task here...")}
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={handleAddTodo}
+          className="hidden bg-indigo-400 dark:bg-slate-900 hover:bg-indigo-300 hover:dark:bg-slate-700 text-white rounded-lg p-4"
+        >
+          ADD
+        </button>
+      </form>
       {todos && (
         <div className="w-full lg:w-1/2 lg:mx-auto flex flex-col gap-2 items-center justify-center py-2">
           <form>
